5993687a498508108b81a832d97c51d5b1838732
Vamshi-krishh <vamshir2001@gmail.com>
2023-04-18 04:32:34 +0530

    Customize Hello World message

---
+++ MyApp.java
@@ -0,0 +1,7 @@
+package com.nagra;
+
+public class MyApp {
+    public static void main(String[] args) {
+        System.out.println("Hello world!! This is Vamshi....Welcome to Nagra");
+    }
+}git 
